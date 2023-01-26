cd68768d0ac1c975826925754ce668983068eff9
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-01-26 11:48:49 +0500

    paussss

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyCurrency(),
+  ));
+}
+
 class MyCurrency extends StatefulWidget {
   const MyCurrency({Key? key}) : super(key: key);
 
@@ -13,162 +20,248 @@ class _MyCurrencyState extends State<MyCurrency> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.black,
         body: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        TextField(
-          decoration: InputDecoration(border: OutlineInputBorder()),
-          keyboardType: TextInputType.none,
-          controller: controller1,
-        ),
-    
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        controller1.text+='7';
-                      });
-                     
-                    },
-                    child: Text('7')),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        controller1.text += '4';
-                      });
-                    },
-                    child: Text('4')),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        controller1.text += '1';
-                      });
-                    },
-                    child: Text('1')),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        controller1.text='';
-                      });
-                    },
-                    child: Text('clear'))
-              ],
+            SizedBox(
+              height: 450,
             ),
-            Column(
-              children: [
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        controller1.text += '8';
-                      });
-                    },
-                    child: Text('8')),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        controller1.text += '5';
-                      });
-                    },
-                    child: Text('5')),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        controller1.text += '2';
-                      });
-                    },
-                    child: Text('2')),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        controller1.text += '0'.toString();
-                      });
-                    },
-                    child: Text('0'))
-              ],
+            TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  filled: true,
+                  fillColor: Colors.white),
+              keyboardType: TextInputType.none,
+              controller: controller1,
             ),
-            Column(
-              children: [
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        controller1.text += '9';
-                      });
-                    },
-                    child: Text('9')),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        controller1.text += '6';
-                      });
-                    },
-                    child: Text('6')),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        controller1.text += '3';
-                      });
-                    },
-                    child: Text('3')),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        controller1.text += '';
-                      });
-                    },
-                    child: Text('='))
-              ],
-            ),
-            Column(
-              children: [
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        controller1.text += '+';
-                      });
-                      setState(() {
-                        num number=num.parse(controller1.text);
-                        controller1.text=(number*number).toString();
-                      });
-                    },
-                    child: Text('+')),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        controller1.text += '-';
-                      });
-                    },
-                    child: Text('-')),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        controller1.text += '  *';
-                      });
-                    },
-                    child: Text('*')),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        controller1.text += '/';
-                      });
-                    },
-                    child: Text('/'))
-              ],
+            Container(
+              color: Colors.white,
+              // height: 400,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange),
+                          onPressed: () {
+                            controller1.text += '7';
+                          },
+                          child: Text('7')),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.orange),
+                        onPressed: () {
+                          controller1.text += '8';
+                        },
+                        child: Text(
+                          '8',
+                        ),
+                      ),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange),
+                          onPressed: () {
+                            controller1.text += '9';
+                          },
+                          child: Text('9')),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Color.fromARGB(255, 121, 166, 245)),
+                          onPressed: () {
+                            controller1.text = '';
+                          },
+                          child: Text('clear'))
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange),
+                          onPressed: () {
+                            controller1.text += '4';
+                          },
+                          child: Text('4')),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange),
+                          onPressed: () {
+                            controller1.text += '5';
+                          },
+                          child: Text('5')),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange),
+                          onPressed: () {
+                            controller1.text += '6';
+                          },
+                          child: Text('6')),
+                      ElevatedButton(
+                          onPressed: () {
+                            style:
+                            ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    Color.fromARGB(255, 121, 166, 245));
+                            controller1.text += '-';
+                          },
+                          child: Text('-'))
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange),
+                          onPressed: () {
+                            controller1.text += '1';
+                          },
+                          child: Text('1')),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange),
+                          onPressed: () {
+                            controller1.text += '2';
+                          },
+                          child: Text('2')),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange),
+                          onPressed: () {
+                            controller1.text += '3';
+                          },
+                          child: Text('3')),
+                      ElevatedButton(
+                          onPressed: () {
+                            style:
+                            ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    Color.fromARGB(255, 121, 166, 245));
+                            int ind = 0;
+                            String s = controller1.text;
+                            ind = s.indexOf('+');
+                            if (ind != -1) {
+                              String a = s.substring(0, ind);
+                              String b = s.substring(ind + 1);
+                              num ans = num.parse(a) + num.parse(b);
+                              controller1.text = ans.toString();
+                            }
+                            ind = s.indexOf('-');
+                            if (ind != -1) {
+                              String a = s.substring(0, ind);
+                              String b = s.substring(ind + 1);
+                              num ans = num.parse(a) - num.parse(b);
+                              controller1.text = ans.toString();
+                            }
+                            ind = s.indexOf('/');
+                            if (ind != -1) {
+                              String a = s.substring(0, ind);
+                              String b = s.substring(ind + 1);
+                              num ans = num.parse(a) / num.parse(b);
+                              controller1.text = ans.toString();
+                            }
+                            ind = s.indexOf('*');
+                            if (ind != -1) {
+                              String a = s.substring(0, ind);
+                              String b = s.substring(ind + 1);
+                              num ans = num.parse(a) * num.parse(b);
+                              controller1.text = ans.toString();
+                            }
+
+                            ind = s.indexOf('%');
+                            if (ind != -1) {
+                              {
+                                String a = s.substring(0, ind);
+                                String b = s.substring(ind + 1);
+                                int ans = (int.parse(b) * 100 ~/ int.parse(a));
+                                controller1.text = ans.toString();
+                              }
+                            }
+                          },
+                          child: Text('='))
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange),
+                          onPressed: () {
+                            controller1.text += '.';
+                          },
+                          child: Text('.')),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange),
+                          onPressed: () {
+                            controller1.text += '0';
+                          },
+                          child: Text('0')),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange),
+                          onPressed: () {
+                            controller1.text += '00';
+                          },
+                          child: Text('00')),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Color.fromARGB(255, 121, 166, 245)),
+                          onPressed: () {
+                            controller1.text += '/';
+                          },
+                          child: Text('/'))
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Color.fromARGB(255, 121, 166, 245)),
+                          onPressed: () {
+                            controller1.text = '0';
+                          },
+                          child: Text('C')),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Color.fromARGB(255, 121, 166, 245)),
+                          onPressed: () {
+                            controller1.text = '';
+                          },
+                          child: Text('AC')),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Color.fromARGB(255, 121, 166, 245)),
+                          onPressed: () {
+                            controller1.text = controller1.text
+                                .substring(0, controller1.text.length - 1);
+                          },
+                          child: Text('X')),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Color.fromARGB(255, 121, 166, 245)),
+                          onPressed: () {
+                            controller1.text += '%';
+                          },
+                          child: Text('%'))
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
-        ),
-      ],
-    ));
+        ));
   }
-}
-
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MyCurrency(),
-  ));
 }
